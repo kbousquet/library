32cdb8674bb116bce71a94db3ea627bdbc15aa96
Kathryn Bousquet <72675497+kbousquet@users.noreply.github.com>
2022-04-29 21:16:42 -0500

    add edits

--- script.js
+++ script.js
@@ -1,28 +1,63 @@
 let myLibrary = [];
+let main = document.getElementById("main");
 
+
+document.querySelector(".add-btn").addEventListener("click", function(){
+  document.querySelector(".popup").classList.add("active");
+});
+
+document.querySelector(".close-btn").addEventListener("click", function(){
+  document.querySelector(".popup").classList.remove("active");
+});
 
 function Book(title, author, pages, read){
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function(){
-      
-      if (this.read === 'yes'){
-        return `${title} by ${author}, ${pages} pages, read`
-      } else {
-        return `${title} by ${author}, ${pages} pages, not read yet`
-      }
-    }
+    this.info = function(){}
 }
+
 
 function addBookToLibrary(){
-  num = currentSize*currentSize
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = "";
+  if (document.getElementById("read").checked){
+    read = "yes";
+  } else{
+    read = "no"
+  }
+  let book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
 
-  for(i=0;i<num;i++){
+
+function displayBooks(){
+  const cards = document.querySelectorAll('.card');
+  cards.forEach(card =>{
+    card.remove();
+  })
+  let length = myLibrary.length; 
+  for(i=0;i<length;i++){
       let card = document.createElement("div");
       card.classList.add("card");
-      canvas.appendChild(card);
+      main.appendChild(card);
     }
- 
 }
+
+
+function resetForm(){
+  document.getElementById("title").value = '';
+  document.getElementById("author").value = '';
+  document.getElementById("pages").value = '';
+  document.getElementById("read").checked = false;
+}
+
+
+document.querySelector(".addBook").addEventListener('click', ()=> {
+  addBookToLibrary();
+  displayBooks();
+  resetForm();
+});
